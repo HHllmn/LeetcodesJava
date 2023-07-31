ed462ede703fc4fbd14cbf9912099a11097c31a9
HHllmn <118361459+HHllmn@users.noreply.github.com>
2023-07-31 19:48:22 +0100

    Time: 1 ms (99.84%), Space: 48.6 MB (43.18%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int val = 0;
+        
+        for (int num : nums){
+            if (count == 0)val = num;
+            if (num == val){
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return val;
+    }
+}
