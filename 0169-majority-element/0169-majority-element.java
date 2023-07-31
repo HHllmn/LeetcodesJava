8e8c8d1ba176b0dc9447dc3ff37727d959183c9a
HHllmn <118361459+HHllmn@users.noreply.github.com>
2023-07-31 19:45:06 +0100

    Time: 1 ms (99.84%), Space: 48.8 MB (23.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,9 +4,7 @@ class Solution {
         int val = 0;
         
         for (int num : nums){
-            if (count == 0){
-                val = num;
-            }
+            if (count == 0)val = num;
             if (num == val){
                 count++;
             } else {
