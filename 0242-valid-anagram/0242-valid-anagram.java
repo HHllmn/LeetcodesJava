e74b3d03a46175da0f30bec8f37c685626eb3d67
HHllmn <118361459+HHllmn@users.noreply.github.com>
2023-08-05 10:28:14 +0100

    Time: 2 ms (99.82%), Space: 43.4 MB (65.52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (char c : s.toCharArray()) alphabet[c-'a']++;
+        for (char c : t.toCharArray()) alphabet[c-'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
+    }
+}
