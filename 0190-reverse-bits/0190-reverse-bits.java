ce97bba4adedc06a4bbda0f4586df29c6ab38f67
HHllmn <118361459+HHllmn@users.noreply.github.com>
2023-08-11 22:36:31 +0100

    Time: 1 ms (80.39%), Space: 40.9 MB (49.09%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -3,7 +3,8 @@ public class Solution {
     public int reverseBits(int n) {
         int result=0;
         for(int i=0;i<32;i++){
-            result = ( result << 1 ) | ( n & 1 );         
+            result = result << 1;
+            result = result | ( n & 1 );         
             n = n >> 1;                  
         }
         return result;
