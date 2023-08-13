e4d0998c01348146a1efa1632147388500b32706
HHllmn <118361459+HHllmn@users.noreply.github.com>
2023-08-13 18:32:27 +0100

    Time: 1 ms (99.43%), Space: 39.6 MB (76.79%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,12 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
         if (x == 0 || x == 1) return x;
-        int i = 1;
-        while(i<=x/i){
-            i++;
-            if(x/i==i) return i;
+        int start = 1;
+        int end = x;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if ((long)mid * mid > (long)x){
+                end = mid - 1;
+            }
+            else if ((long)mid * mid == (long)x){
+                return mid;
+            }
+            else{
+                start = mid + 1;
+            }
         }
-        return i-1;
+        return end;
         
     }
 }
