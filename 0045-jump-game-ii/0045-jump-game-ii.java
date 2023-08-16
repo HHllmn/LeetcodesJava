b336fa44e1ca9c34db14b23a927141551991dcc6
HHllmn <118361459+HHllmn@users.noreply.github.com>
2023-08-16 16:49:58 +0100

    Time: 1 ms (99.24%), Space: 44.7 MB (50.25%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int steps = 0;
+        int maxReach = 0;
+        int end = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (i == end) {
+                steps++;
+                end = maxReach;
+            }
+        }
+        return steps;
+    }
+}
