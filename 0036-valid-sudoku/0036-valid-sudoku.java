113664e40704e7178f71a9d2abf65150dc254dd5
HHllmn <118361459+HHllmn@users.noreply.github.com>
2023-08-24 10:20:48 +0100

    Time: 1 ms (100%), Space: 42.8 MB (95.35%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,14 +1,33 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set seen = new HashSet();
         for (int i = 0; i < 9; i++){
             for (int j = 0; j < 9; j++){
                 if (board[i][j] != '.'){
-                    String numAtPos = "(" + board[i][j] + ")";
-                    if (!seen.add(numAtPos + i) || !seen.add(j + numAtPos) || !seen.add(i/3 + numAtPos + j/3)) return false;
+                    if(!isValid(board,i,j,board[i][j])) return false;
                 }
             }
         }
         return true;
     }
+
+    public boolean isValid(char[][] board, int row, int col, char num){
+        for(int i=col+1;i<9;i++){
+            if(board[row][i] == num)
+                return false;
+        }
+        
+        for(int i=row+1;i<9;i++){
+            if(board[i][col] == num)
+                return false;
+        }
+        
+        int x = row - row%3, y = col-col%3;
+        for(int i=x; i<x+3; i++){
+            for(int j=y; j<y+3; j++){
+                if(board[i][j]==num && !(i==row && j==col))
+                    return false;
+            }
+        }   
+        return true;
+    }   
 }
