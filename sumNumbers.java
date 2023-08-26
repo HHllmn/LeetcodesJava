fc803e0686189fd2948cc3c4e3018dedc8002108
HHllmn <118361459+HHllmn@users.noreply.github.com>
2023-08-26 11:29:54 +0100

    Sum Root to Leaf Numbers problem

---
+++ sumNumbers.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return sumHelper(root, 0);
+    }
+
+    public int sumHelper(TreeNode root, int num){
+        if (root == null) return 0;
+        if (root.right == null && root.left == null) return num*10 + root.val;
+        return sumHelper(root.left, num*10 + root.val) + sumHelper(root.right, num*10 + root.val);
+    }
+}
